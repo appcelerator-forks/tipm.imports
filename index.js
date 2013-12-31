b7a7b7e2d687094a25f9efe59ea6d9437e9e3fc9
Christian Sullivan <cs@euforic.co>
2013-12-31 14:23:23 -0800

    update syntax

--- index.js
+++ index.js
@@ -7,17 +7,21 @@
 
 var globalCTX = (function(){ return this; })();
 
-
-var imports = module.exports = {}
+var imports = module.exports = {};
 
 var _aliase = {};
 
 imports.aliase = function(name, aliase){
-  if ('string' === instanceof name) return _aliase[name] = aliase;
+  if ('string' === instanceof name){
+    _aliase[name] = aliase;
+    return;
+  }
+
   for (var key in name){
     _aliase[key] = name[key];
   }
-}
+
+};
 
 
 // append imports function to global context
